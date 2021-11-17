75f2f9df854242223a813cc32f3bd930e0f4be40
QueziaFonseca <qfonseca1@gmail.com>
2021-11-17 14:26:09 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,9 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-    // add your implementation here
-
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    soma += array[i];
+  }
+  return Math.round(soma / array.length);
 };
 
+console.log(average([0, 0, 0, 0, 0, 0, 1]));
 module.exports = average;
