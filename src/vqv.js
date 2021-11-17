efd2d4324b3da9e658845c10b199545f05ab02f0
QueziaFonseca <qfonseca1@gmail.com>
2021-11-17 19:36:17 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,15 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = () => {};
+// na vdd, quando esta sem parametro ja é undefined
+const vqv = (string, number) => {
+  if (typeof string === 'undefined' || typeof number === 'undefined') {
+    return undefined;
+  }
+  return ` 'Oi, meu nome é ${string}!
+  Tenho ${number} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!'`;
+};
 
 module.exports = vqv;
