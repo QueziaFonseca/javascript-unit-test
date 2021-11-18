361da7c2eef9f1fe7267731b8a4656aba61eac41
QueziaFonseca <qfonseca1@gmail.com>
2021-11-17 21:52:13 -0300

    corrige requisito1: corrige template literal

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,10 @@ const vqv = (string, number) => {
   if (typeof string === 'undefined' || typeof number === 'undefined') {
     return undefined;
   }
-  return ` 'Oi, meu nome é ${string}!
-  Tenho ${number} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!'`;
+  return `Oi, meu nome é ${string}!
+Tenho ${number} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
